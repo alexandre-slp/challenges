2a59cedf731be4dd01d0622adc3514d42a7ebabd
Alexandre Paes <alexandre.slp90@gmail.com>
2024-08-09 18:26:06 -0300

    added code and completed tests

--- tasks/python/complete_tests.py
+++ tasks/python/complete_tests.py
@@ -1,1 +1,209 @@
-# Todo: criar testes para todos os cenarios possiveis
+import pytest
+from solutions.linked_list import Task, TasksManager
+
+
+@pytest.fixture
+def tm():
+    task_a = Task('a')
+    task_a.next = task_b = Task('b')
+    task_b.next = Task('c')
+
+    task_manager = TasksManager()
+    task_manager.first_task = task_a
+    task_manager.tasks_quantity = 3
+    return task_manager
+
+
+@pytest.mark.add_task
+class TestAddTask:
+    def test_add_task_first_task(self):
+        tm = TasksManager()
+        tm.add_task('task')
+        assert tm.tasks_quantity == 1
+        assert tm.first_task.name == 'task'
+        assert tm.first_task.description == ''
+
+    def test_add_task_without_name(self):
+        tm = TasksManager()
+        tm.add_task('')
+        assert tm.tasks_quantity == 0
+        assert tm.first_task is None
+
+    def test_add_task_without_position(self):
+        tm = TasksManager()
+        tm.add_task('task1')
+        tm.add_task('task2')
+        assert tm.tasks_quantity == 2
+        assert tm.first_task.name == 'task1'
+        assert tm.first_task.next.name == 'task2'
+
+    def test_add_task_at_first_position(self, tm):
+        tm.add_task('task', add_position=0)
+        assert tm.tasks_quantity == 4
+        assert tm.first_task.name == 'task'
+        assert tm.first_task.next.name == 'a'
+        assert tm.first_task.next.next.name == 'b'
+        assert tm.first_task.next.next.next.name == 'c'
+
+    def test_add_task_at_second_position(self, tm):
+        tm.add_task('task', add_position=1)
+        assert tm.tasks_quantity == 4
+        assert tm.first_task.name == 'a'
+        assert tm.first_task.next.name == 'task'
+        assert tm.first_task.next.next.name == 'b'
+        assert tm.first_task.next.next.next.name == 'c'
+
+    def test_add_task_at_last_position(self, tm):
+        tm.add_task('task', add_position=4)
+        assert tm.tasks_quantity == 4
+        assert tm.first_task.name == 'a'
+        assert tm.first_task.next.name == 'b'
+        assert tm.first_task.next.next.name == 'c'
+        assert tm.first_task.next.next.next.name == 'task'
+
+    def test_add_task_negative_position(self, tm):
+        tm.add_task('task', add_position=-1)
+        assert tm.tasks_quantity == 3
+        assert tm.first_task.name == 'a'
+        assert tm.first_task.next.name == 'b'
+        assert tm.first_task.next.next.name == 'c'
+
+    def test_add_task_invalid_position(self, tm):
+        tm.add_task('task', add_position=10)
+        assert tm.tasks_quantity == 3
+        assert tm.first_task.name == 'a'
+        assert tm.first_task.next.name == 'b'
+        assert tm.first_task.next.next.name == 'c'
+
+    def test_add_task_same_name(self, tm):
+        tm.add_task('a')
+        assert tm.tasks_quantity == 3
+
+
+@pytest.mark.remove_task
+class TestRemoveTask:
+    def test_remove_first_task(self, tm):
+        removed_task = tm.remove_task('a')
+        assert removed_task.name == 'a'
+        assert tm.tasks_quantity == 2
+        assert tm.first_task.name == 'b'
+        assert tm.first_task.next.name == 'c'
+
+    def test_remove_second_task(self, tm):
+        removed_task = tm.remove_task('b')
+        assert removed_task.name == 'b'
+        assert tm.tasks_quantity == 2
+        assert tm.first_task.name == 'a'
+        assert tm.first_task.next.name == 'c'
+
+    def test_remove_last_task(self, tm):
+        removed_task = tm.remove_task('c')
+        assert removed_task.name == 'c'
+        assert tm.tasks_quantity == 2
+        assert tm.first_task.name == 'a'
+        assert tm.first_task.next.name == 'b'
+
+    def test_remove_non_existent_task(self, tm):
+        removed_task = tm.remove_task('task')
+        assert removed_task is None
+        assert tm.tasks_quantity == 3
+        assert tm.first_task.name == 'a'
+        assert tm.first_task.next.name == 'b'
+        assert tm.first_task.next.next.name == 'c'
+
+    def test_remove_task_empty(self):
+        tm = TasksManager()
+        removed_task = tm.remove_task('task')
+        assert removed_task is None
+        assert tm.tasks_quantity == 0
+
+
+@pytest.mark.find_task
+class TestFindTask:
+    def test_find_task_first_position(self, tm):
+        position = tm.find_task('a')
+        assert position == 0
+
+    def test_find_task_second_position(self, tm):
+        position = tm.find_task('b')
+        assert position == 1
+
+    def test_find_task_last_position(self, tm):
+        position = tm.find_task('c')
+        assert position == 2
+
+    def test_find_non_existent_task(self, tm):
+        position = tm.find_task('d')
+        assert position == -1
+
+    def test_find_task_empty(self, tm):
+        position = tm.find_task('d')
+        assert position == -1
+
+
+@pytest.mark.move_task
+class TestMoveTask:
+    def test_move_first_task_to_first(self, tm):
+        tm.move_task('a', 0)
+        assert tm.find_task('a') == 0
+
+    def test_move_first_task_to_second(self, tm):
+        tm.move_task('a', 1)
+        assert tm.find_task('a') == 1
+
+    def test_move_first_task_to_last(self, tm):
+        tm.move_task('a', 2)
+        assert tm.find_task('a') == 2
+
+    def test_move_second_task_to_first(self, tm):
+        tm.move_task('b', 0)
+        assert tm.find_task('b') == 0
+
+    def test_move_second_task_to_second(self, tm):
+        tm.move_task('b', 1)
+        assert tm.find_task('b') == 1
+
+    def test_move_second_task_to_last(self, tm):
+        tm.move_task('b', 2)
+        assert tm.find_task('b') == 2
+
+    def test_move_last_task_to_first(self, tm):
+        tm.move_task('c', 0)
+        assert tm.find_task('c') == 0
+
+    def test_move_last_task_to_second(self, tm):
+        tm.move_task('c', 1)
+        assert tm.find_task('c') == 1
+
+    def test_move_last_task_to_last(self, tm):
+        tm.move_task('c', 2)
+        assert tm.find_task('c') == 2
+
+    def test_move_non_existent_task(self, tm):
+        tm.move_task('task', 0)
+        assert tm.find_task('task') == -1
+
+
+@pytest.mark.list_task
+class TestListTasks:
+    def test_list_tasks(self, tm):
+        tasks = tm.list_tasks()
+        assert len(tasks) == 3
+        assert tasks[0].name == 'a'
+        assert tasks[1].name == 'b'
+        assert tasks[2].name == 'c'
+
+    def test_list_tasks_only_one(self):
+        tm = TasksManager()
+        tm.add_task('task')
+        tasks = tm.list_tasks()
+        assert tasks[0].name == 'task'
+
+    def test_list_tasks_empty(self):
+        tm = TasksManager()
+        tasks = tm.list_tasks()
+        assert tasks == []
+
+
+if __name__ == "__main__":
+    pytest.main()
--- tasks/python/solutions/linked_list.py
+++ tasks/python/solutions/linked_list.py
@@ -1,1 +1,126 @@
-# Criar solução de lista encadeada
+# Criar solução de lista simplesmente encadeada
+
+class Task:
+    def __init__(self, name: str, description=''):
+        self.name = name.lower()
+        self.description = description.lower()
+        self.next = None
+
+
+class TasksManager:
+    first_task = None
+    tasks_quantity = 0
+
+    def add_task(self, name: str, description='', add_position=None):
+        if not name:
+            return
+
+        if add_position is None:
+            add_position = self.tasks_quantity + 1
+
+        if add_position < 0 or add_position > self.tasks_quantity + 1:
+            return
+
+        if self.find_task(name) != -1:
+            return
+
+        new_task = Task(name, description)
+        self.tasks_quantity += 1
+        if self.first_task is None:
+            self.first_task = new_task
+            return
+
+        if add_position == 0:
+            new_task.next = self.first_task
+            self.first_task = new_task
+            return
+
+        position = 0
+        previous = None
+        task = self.first_task
+        while task.next is not None:
+            if position == add_position:
+                previous.next = new_task
+                new_task.next = task
+                return
+
+            previous = task
+            task = task.next
+            position += 1
+
+        if position == add_position:
+            previous.next = new_task
+            new_task.next = task
+            return
+
+        task.next = new_task
+
+    def remove_task(self, name: str):
+        if self.tasks_quantity == 0:
+            return
+
+        previous = None
+        position = 0
+        task = self.first_task
+        while task.next is not None:
+            if task.name == name:
+                self.tasks_quantity -= 1
+                if position == 0:
+                    self.first_task = task.next
+                    return task
+
+                previous.next = task.next
+                return task
+
+            previous = task
+            task = task.next
+            position += 1
+
+        if task.name == name:
+            previous.next = None
+            self.tasks_quantity -= 1
+            return task
+
+    def find_task(self, name: str):
+        if self.tasks_quantity == 0:
+            return -1
+
+        position = 0
+        task = self.first_task
+        while task.next is not None:
+            if task.name == name:
+                return position
+
+            position += 1
+            task = task.next
+
+        if task.name == name:
+            return position
+
+        return -1
+
+    def move_task(self, name: str, new_position: int):
+        if self.tasks_quantity == 0:
+            return
+
+        if new_position < 0 or new_position > self.tasks_quantity - 1:
+            return
+
+        moved_task = self.remove_task(name)
+        if moved_task is None:
+            return
+
+        self.add_task(moved_task.name, moved_task.description, new_position)
+
+    def list_tasks(self):
+        if self.first_task is None:
+            return []
+
+        tasks = []
+        task = self.first_task
+        while task.next is not None:
+            tasks.append(task)
+            task = task.next
+
+        tasks.append(task)
+        return tasks
